ef0949bd6527d446952a2942b5cc8d63a20b0fe7
ayyasmumtaz <yazmumtaz@gmail.com>
2023-12-11 09:23:05 +0700

    update puzzle

--- puzzle.py
+++ puzzle.py
@@ -1,19 +1,27 @@
 from logic import *
 
-AKnight = Symbol("A is a Knight")
-AKnave = Symbol("A is a Knave")
+AKnight = Symbol("A adalah Knight")
+AKnave = Symbol("A adalah Knave")
+BKnight = Symbol("B adalah Knight")
+BKnave = Symbol("B adalah Knave")
+CKnight = Symbol("C adalah Knight")
+CKnave = Symbol("C adalah Knave")
 
-BKnight = Symbol("B is a Knight")
-BKnave = Symbol("B is a Knave")
 
-CKnight = Symbol("C is a Knight")
-CKnave = Symbol("C is a Knave")
+knowledgeBase = And(
+    Or(AKnight,AKnave),
+    Or(BKnight,BKnave),
+    Or(CKnight,CKnave),
+    Not(And(AKnight,AKnave)),
+    Not(And(BKnight,BKnave)),
+    Not(And(CKnight,CKnave))
+)
 
 # Puzzle 0
 # A says "I am both a knight and a knave."
 knowledge0 = And(
     # A is a knight or a knave but not both:
-    And(Or(AKnight, AKnave), Not(And(AKnight, AKnave))),
+    knowledgeBase,
     # If A is a knight his sentence is true:
     Implication(AKnight, And(AKnight, AKnave)),
     # If A is a knave his sentence is false:
@@ -25,8 +33,7 @@ knowledge0 = And(
 # B says nothing.
 knowledge1 = And(
     # A and B are knights or knaves but not both:
-    And(Or(AKnight, AKnave), Not(And(AKnight, AKnave))),
-    And(Or(BKnight, BKnave), Not(And(BKnight, BKnave))),
+    knowledgeBase,
     # If A is a knight, A and B are both knaves:
     Implication(AKnight, And(AKnave, BKnave)),
     # If A is a knave, his statement is false:
@@ -38,8 +45,7 @@ knowledge1 = And(
 # B says "We are of different kinds."
 knowledge2 = And(
     # A and B are knights or knaves but not both:
-    And(Or(AKnight, AKnave), Not(And(AKnight, AKnave))),
-    And(Or(BKnight, BKnave), Not(And(BKnight, BKnave))),
+    knowledgeBase,
     # If A is a knight, A and B are both the same:
     Implication(AKnight, Or(And(AKnight, BKnight), And(AKnave, BKnave))),
     # If A is a knave, his statement is false:
@@ -50,54 +56,28 @@ knowledge2 = And(
     Implication(BKnave, Not(Or(And(AKnight, BKnave), And(AKnave, BKnight))))
 )
 
-# Puzzle 3
-# A says either "I am a knight." or "I am a knave.", but you don't know which.
-# B says "A said 'I am a knave'."
-# B says "C is a knave."
-# C says "A is a knight."
-knowledge3 = And(
-    # A, B and C are knights or knaves but not both:
-    And(Or(AKnight, AKnave), Not(And(AKnight, AKnave))),
-    And(Or(BKnight, BKnave), Not(And(BKnight, BKnave))),
-    And(Or(CKnight, CKnave), Not(And(CKnight, CKnave))),
-    # If B is a knight, A said 'I am a knave', and C is a knave:
-    Implication(BKnight, CKnave),
-    Implication(BKnight, And(
-      # A then said 'I am a Knave', A may be a Knight or a Knave:
-      Implication(AKnight, AKnave),
-      Implication(AKnave, Not(AKnave)),
-    )),
-    # If B is a knave, A said 'I am a knight' C is not a knave:
-    Implication(BKnave, Not(CKnave)),
-    Implication(BKnave, And(
-      # A then said 'I am a Knight', A may be a Knight or a Knave:
-      Implication(AKnight, AKnight),
-      Implication(AKnave, Not(AKnight))
-    )),
-    # If C is a knight, A is a knight:
-    Implication(CKnight, AKnight),
-    # If C is a knave, A is not a knight:
-    Implication(CKnave, Not(AKnight))
-)
 
 
 def main():
     symbols = [AKnight, AKnave, BKnight, BKnave, CKnight, CKnave]
     puzzles = [
-        ("Puzzle 0", knowledge0),
-        ("Puzzle 1", knowledge1),
-        ("Puzzle 2", knowledge2),
-        ("Puzzle 3", knowledge3)
+        ("Puzzle Pertama", knowledge0),
+        ("Puzzle Kedua", knowledge1),
+        ("Puzzle Ketiga", knowledge2),
     ]
+    print("------------------------------------SELAMAT DATANG DI PERMAINAN KNIGHT AND KNAVE------------------------------------")
+    print("Berikut merupakan jawaban dari puzzle yang kamu buat:")
+    print()
     for puzzle, knowledge in puzzles:
-        print(puzzle)
+        print(f"{puzzle} :")
         if len(knowledge.conjuncts) == 0:
-            print("    Not yet implemented.")
+            print("")
         else:
             for symbol in symbols:
                 if model_check(knowledge, symbol):
                     print(f"    {symbol}")
-
-
+        print("-------------------------------------------------------------------------------------------------------------------")
+    print()
+    print("----------------------------------------------------TERIMAKASIH----------------------------------------------------")
 if __name__ == "__main__":
     main()
